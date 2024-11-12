cb5e90ecaccafe9e7895c9193b01b59a21fc2bcb
MagongDo <lpok2401@naver.com>
2024-11-12 15:04:57 +0900

    asd

---
+++ testing/src/main/java/com/example/testing/dto/TodoDto.java
@@ -0,0 +1,35 @@
+package com.example.testing.dto;
+
+
+import com.example.testing.domain.Todo;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoDto {
+    Long id;
+    String title;
+    String description;
+
+    public static TodoDto of(Todo todo) {
+        return TodoDto.builder()
+                .id(todo.getId())
+                .title(todo.getTitle())
+                .description(todo.getDescription())
+                .build();
+    }
+
+    public Todo toEntity() {
+        return Todo.builder()
+                .id(id)
+                .title(title)
+                .description(description)
+                .build();
+    }
+
+}
--- testing/src/main/java/com/example/testing/service/TodoService.java
+++ testing/src/main/java/com/example/testing/service/TodoService.java
@@ -1,36 +1,34 @@
 package com.example.testing.service;
 
 import com.example.testing.domain.Todo;
+import com.example.testing.dto.TodoDto;
 import com.example.testing.repository.TodoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class TodoService {
-    private final TodoRepository todoRepository;
+    final TodoRepository repository;
 
-    public TodoService(TodoRepository todoRepository) {
-        this.todoRepository = todoRepository;
-    }
-
-    public void createTodo(Todo todo) {
-
-        todoRepository.save(todo);
+    public TodoDto create(TodoDto dto) {
+        Todo todo = dto.toEntity();
+        Todo savedTodo = repository.save(todo);
+        return TodoDto.of(savedTodo);
     }
 
-    public List<Todo> getTodos() {
-        return todoRepository.findAll();
+    public TodoDto get(Long id) {
+        return repository.findById(id).map(TodoDto::of)
+                .orElseThrow(NoSuchElementException::new);
     }
-    public Optional<Todo> getTodo(Long id) {
-        return todoRepository.findById(id);
-    }
-    public void deleteTodo(Long id) {
-        todoRepository.deleteById(id);
-    }
-    public void updateTodo(Todo todo) {
-        Todo todos=Todo.builder().title("바꿔").description("막바꿔").build();
-        todoRepository.save(todos);
+
+    public List<TodoDto> getList() {
+        return repository.findAll().stream()
+                .map(TodoDto::of)
+                .toList();
     }
 }
